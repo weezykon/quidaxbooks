442ca2d6d3397b8b30b04e12500f637f2a075f48
Akinbode Akinlolu Weezykon <weezykon@gmail.com>
2021-09-14 11:16:43 +0100

    changed permission

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+// apollo
+import { ApolloClient, ApolloProvider, HttpLink, from, InMemoryCache } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
 import logo from './logo.svg';
 import './styles/app.scss';
 import { Route } from 'react-router-dom';
@@ -13,9 +17,44 @@ const routes = [
   { path: '/book', name: 'Book', Component: Book },
 ];
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(`Graphql Error ${message}`)
+    );
+  }
+  if (networkError) console.log(`Network Error ${networkError}`);
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+]);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
+
+// const App = () => {
+//   return (
+//     <ApolloProvider client={client}>
+//       <div className="App">
+//         <Header />
+//         <div className="container">
+//           <Route path="/" exact component={Home} />
+//           <Route path="/book" exact component={Book} />
+//         </div>
+//       </div>
+//     </ApolloProvider>
+//   );
+// };
+
+// export default App;
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Header />
       <main className="container">
         {routes.map(({ path, Component }) => (
@@ -24,7 +63,7 @@ function App() {
           </Route>
         ))}
       </main>
-    </>
+    </ApolloProvider>
   );
 }
 
