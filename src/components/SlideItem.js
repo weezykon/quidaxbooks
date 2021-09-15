768e60d116e12f66d1b9ff5f44819f07c1a26a4d
Akinbode Akinlolu Weezykon <weezykon@gmail.com>
2021-09-15 14:36:32 +0100

    featured overlay

--- src/components/SlideItem.js
+++ src/components/SlideItem.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { Rating } from 'react-simple-star-rating'
 import { NavLink } from 'react-router-dom';
+import userIcon from './../assets/images/userWhite.svg';
+import heartIcon from './../assets/images/heartWhite.svg';
 
 const SlideItem = ({ book }) => {
     return (
@@ -7,6 +10,42 @@ const SlideItem = ({ book }) => {
             <NavLink to={'book/' + book.id}>
                 <div className="slide-item">
                     <img src={book.image_url} alt={book.title} />
+                    <div className="slide-item-details">
+                        <div className="available">
+                            {book.available_copies > 0 ? (
+                                <p className="text-green">Available</p> 
+                            ): (
+                                <p className="text-red">Out of Stock</p>
+                            )}
+                        </div>
+                        <div className="author">
+                            <h3>{book.title}</h3>
+                            <p>{book.authors.map(genre => genre.name).join(', ')}</p>
+                            <p className="year">{book.published_at.slice(0, 4)}</p>
+                        </div>
+                        <div className="genre">
+                            <p>Genre</p>
+                            <p>{book.genres.map(genre => genre.name).join(', ')}</p>
+                        </div>
+                        <div className="tags">
+                            <p>Tags</p>
+                            <p>{book.tags.map(tag => tag.name).join(', ')}</p>
+                        </div>
+                        <div className="rating">
+                            <div className="flex flex-column user">
+                                <img src={userIcon} alt="user" />
+                                <p>{book.number_of_purchases}</p>
+                            </div>
+                            <div className="flex flex-column heart mx-3">
+                                <img src={heartIcon} alt="heart" />
+                                <p>{book.likes}</p>
+                            </div>
+                            <div className="flex flex-column rating-box">
+                                <p><b>Rating:</b> {book.rating} </p>
+                                <Rating ratingValue={Math.round(book.rating)} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </NavLink>
         </>
